20f890d331afcc24b4390f03630efcbf77ac93a0
Linkerin <79026567+Linkerin@users.noreply.github.com>
2023-03-17 22:33:58 +0300

    UPD: added sizes for UserAvatar component

--- components/UserInfo/Avatars/UserAvatar.tsx
+++ components/UserInfo/Avatars/UserAvatar.tsx
@@ -1,30 +1,65 @@
 import { Box, Image } from '@chakra-ui/react';
 
-import { AuthContext } from '@/context/AuthContext';
 import UserIcon from '@/components/Icons/UserIcon';
 import GuestAvatarSvg from './GuestAvatarSvg';
 // ADD credits to guest avatar svg
 // <a href='https://www.freepik.com/vectors/cute-sloth'>Cute sloth vector created by catalyststuff - www.freepik.com</a>
 
+const sizes = {
+  xs: {
+    width: 34,
+    height: 34,
+    borderWidth: '1px',
+    padding: 2
+  },
+  sm: {
+    width: 50,
+    height: 50,
+    borderWidth: '1px',
+    padding: 3
+  },
+  md: {
+    width: 80,
+    height: 80,
+    borderWidth: '2px',
+    padding: 4
+  },
+  lg: {
+    width: 100,
+    height: 100,
+    borderWidth: '2px',
+    padding: 5
+  }
+};
+
 interface UserAvatarProps {
   avatar?: string;
   username?: string;
-  small?: boolean;
+  size?: keyof typeof sizes;
+  disabled?: boolean;
+  noBorder?: boolean;
 }
 
-function UserAvatar({ avatar, username, small = false }: UserAvatarProps) {
-  const AvatarContainer = ({ children }: { children: React.ReactElement }) => {
+function UserAvatar({
+  avatar,
+  username,
+  size = 'lg',
+  disabled = false,
+  noBorder = true
+}: UserAvatarProps) {
+  const AvatarContainer = ({ children }: { children: React.ReactNode }) => {
     return (
       <Box
         css={{
-          borderWidth: '2px',
+          borderWidth: sizes[size].borderWidth,
           borderStyle: 'solid',
           borderColor: 'currentColor',
           borderRadius: '50%',
+          border: noBorder ? 'none' : undefined,
           margin: 0,
-          padding: 5,
-          height: 100,
-          width: 100,
+          padding: sizes[size].padding,
+          height: sizes[size].height,
+          width: sizes[size].width,
           svg: {
             width: '100%',
             height: 'auto',
@@ -44,7 +79,9 @@ function UserAvatar({ avatar, username, small = false }: UserAvatarProps) {
 
   return (
     <AvatarContainer>
-      {avatar ? (
+      {disabled ? (
+        <UserIcon bgColor="gray.200" p={sizes[size].padding / 4} />
+      ) : avatar ? (
         <Image
           src={avatar}
           alt={`${username} avatar`}
