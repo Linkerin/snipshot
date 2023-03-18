b2e65baf75301b4b93f8cb73dc0a08b0c96d4f89
Linkerin <79026567+Linkerin@users.noreply.github.com>
2023-03-18 19:16:09 +0300

    ADD: snippet verification

--- pages/snippets/[lang]/[snippet].tsx
+++ pages/snippets/[lang]/[snippet].tsx
@@ -1,18 +1,24 @@
-import { createContext } from 'react';
+import { createContext, useContext, useState } from 'react';
 import { GetServerSideProps } from 'next';
-import { Grid, GridItem } from '@chakra-ui/react';
+import { Button, Grid, GridItem } from '@chakra-ui/react';
 
+import { AuthContext } from '@/context/AuthContext';
+import CheckedIcon from '@/components/Icons/CheckedIcon';
 import { cleanObjDataTypesForNextJS } from '@/services/utils';
 import CommentsContainer from '@/components/Comments/CommentsContainer';
 import getBySlug from '@/services/prisma/snippetsService/getBySlug';
 import Meta from '@/components/Meta/Meta';
 import Snippet from '@/components/Snippet/Snippet';
 import { SnippetType } from '@/services/types';
+import supabase from '@/services/supabase';
 
 export const SnippetIdContext = createContext<string | null>(null);
 
 function SnippetPage({ snippetData }: { snippetData: SnippetType[] }) {
   const [snippet] = snippetData;
+  const [verifying, setVerifying] = useState(false);
+
+  const user = useContext(AuthContext);
 
   let description = `Page of "${snippet.title}" snippet source code in ${snippet.lang}.`;
   if (snippet.author?.name) {
@@ -20,6 +26,24 @@ function SnippetPage({ snippetData }: { snippetData: SnippetType[] }) {
   }
 
   const keywordsTags = snippet.tags?.join(', ');
+
+  const handleVerification = async () => {
+    if (!user?.appRole?.includes('admin')) return;
+
+    setVerifying(true);
+    try {
+      const { data, error } = await supabase
+        .from('snippets')
+        .update({ verified: true })
+        .eq('id', snippet.id);
+      if (error) throw error;
+    } catch (err) {
+      console.error(`Error while verifying snippet ID ${snippet.id}`);
+      console.error(err);
+    } finally {
+      setVerifying(false);
+    }
+  };
 
   return (
     <>
@@ -33,6 +57,20 @@ function SnippetPage({ snippetData }: { snippetData: SnippetType[] }) {
       <Grid templateColumns="4fr 3fr" gap={1}>
         <GridItem>
           <Snippet snippet={snippet} />
+          {!snippet.verified && user?.appRole?.includes('admin') && (
+            <Button
+              rightIcon={<CheckedIcon boxSize={4} />}
+              size="sm"
+              colorScheme="green"
+              variant="outline"
+              px={2}
+              mt={3}
+              isLoading={verifying}
+              onClick={handleVerification}
+            >
+              Verify snippet
+            </Button>
+          )}
         </GridItem>
         <GridItem h="60vh">
           <SnippetIdContext.Provider value={snippet.id}>
