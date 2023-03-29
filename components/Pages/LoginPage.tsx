271016d0d7e1c485c5afa64587675068b7c62d80
Linkerin <79026567+Linkerin@users.noreply.github.com>
2023-03-29 19:57:46 +0300

    ADD: Text and styling for login page

--- components/Pages/LoginPage.tsx
+++ components/Pages/LoginPage.tsx
@@ -1,5 +1,14 @@
 import { SyntheticEvent, useState } from 'react';
-import { Button, Center, Spinner, useToast } from '@chakra-ui/react';
+import {
+  Button,
+  Center,
+  Spinner,
+  Heading,
+  Highlight,
+  useColorModeValue,
+  useToast,
+  VStack
+} from '@chakra-ui/react';
 
 import GithubIcon from '@/components/Icons/GithubIcon';
 import supabase from '@/services/supabase';
@@ -32,9 +41,46 @@ function LoginPage() {
   };
 
   return (
-    <Center h="100%">
+    <Center h="80%" flexDirection="column">
+      <VStack w={{ base: '75vw', lg: '18vw' }} mb={12} spacing={7}>
+        <Heading
+          as="p"
+          textAlign="left"
+          size={{ base: 'md', lg: 'lg' }}
+          textColor="primary"
+        >
+          <Highlight
+            query="GitHub"
+            styles={{
+              px: 3,
+              py: 0.5,
+              borderRadius: '20px',
+              bgColor: useColorModeValue('custom-black', 'custom-white'),
+              color: useColorModeValue('custom-white', 'custom-black')
+            }}
+          >
+            We use GitHub as a secure and reliable option for authentication
+            process.
+          </Highlight>
+        </Heading>
+        <Heading
+          as="p"
+          textAlign="right"
+          size={{ base: 'sm', lg: 'md' }}
+          textColor="text-secondary"
+        >
+          Do not hesitate to join GitHub community if you don&apos;t have an
+          account yet!
+        </Heading>
+      </VStack>
       {user?.id ? (
-        <Spinner thickness="5px" emptyColor="gray.200" size="xl" speed="0.5s" />
+        <Spinner
+          thickness="5px"
+          color="primary"
+          emptyColor="gray.200"
+          size="xl"
+          speed="0.5s"
+        />
       ) : (
         <Button
           leftIcon={<GithubIcon boxSize={8} />}
