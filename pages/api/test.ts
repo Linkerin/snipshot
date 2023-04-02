df778e08eb229790c999b77713ff83191231be18
Linkerin <79026567+Linkerin@users.noreply.github.com>
2023-04-02 15:56:24 +0300

    TEST: one more test with IPs

--- pages/api/test.ts
+++ pages/api/test.ts
@@ -6,22 +6,19 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  let ipAddress = req.headers['x-real-ip'];
+  // Getting user's original IP address
+  let ipAddress = req.headers['x-real-ip'] as string;
   const forwardedFor = req.headers['x-forwarded-for'] as string;
-  console.log(forwardedFor);
   if (!ipAddress && forwardedFor) {
-    ipAddress = forwardedFor?.split(',').at(0) ?? 'Unknown';
+    ipAddress = forwardedFor?.split(',')[0] ?? '';
   }
-
-  console.log(ipAddress);
-  return res.status(200).json({ forwardedFor, ipAddress });
 
   if (!ipAddress) {
     return res.status(400).json({ message: 'Bad Request' });
   }
 
   const value = await redis.get<number>(ipAddress);
-  console.log(value);
+
   if (!value) {
     await redis.set(ipAddress, Date.now());
     redis.expire(ipAddress, 60);
