74e837067c311f0c65a4678e72eccc07521364f7
Linkerin <79026567+Linkerin@users.noreply.github.com>
2023-04-02 15:28:47 +0300

    TEST: redis config amendments

--- services/redis.ts
+++ services/redis.ts
@@ -2,8 +2,8 @@ import { Redis } from '@upstash/redis';
 import https from 'https';
 
 const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_URL ?? '',
-  token: process.env.UPSTASH_REDIS_TOKEN ?? '',
+  url: process.env.UPSTASH_REDIS_REST_URL ?? '',
+  token: process.env.UPSTASH_REDIS_REST_TOKEN ?? '',
   agent: new https.Agent({ keepAlive: true })
 });
 
