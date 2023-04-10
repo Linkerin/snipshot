112626fd2b0e3e959c87b7797eee4d8d8251db47
Linkerin <79026567+Linkerin@users.noreply.github.com>
2023-04-10 23:36:24 +0300

    ADD: custom 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,70 @@
+import NextLink from 'next/link';
+import { Button, Flex, Heading, Img, Text, Show } from '@chakra-ui/react';
+
+function Custom404() {
+  return (
+    <Flex
+      flexDirection="column"
+      alignItems="center"
+      mt={{ base: 2, md: 5 }}
+      gap={4}
+    >
+      <Heading
+        fontSize={{ base: '7rem', md: '9rem', lg: '12rem' }}
+        color="primary"
+      >
+        404
+      </Heading>
+      <Flex
+        flexDirection="row"
+        alignItems="flex-start"
+        justifyContent="center"
+        gap={{ base: 2, md: 5 }}
+      >
+        <Flex
+          flexDirection="column"
+          w={{ base: '100%', lg: '40%', xl: '60%' }}
+          mx={5}
+          gap={4}
+          alignItems="flex-start"
+          justifyContent="center"
+        >
+          <Heading
+            as="q"
+            size={{ base: 'lg', lg: 'xl' }}
+            fontStyle="italic"
+            letterSpacing="wide"
+          >
+            Everything is rough in the Unknown Regions
+          </Heading>
+          <Text>
+            You are trying to find something that is beyond our reach. Maybe
+            someday.
+          </Text>
+          <Button
+            as={NextLink}
+            href="/"
+            aria-label="Homepage"
+            variant="outline"
+            prefetch={false}
+            alignSelf="flex-start"
+            mt={4}
+            size="lg"
+          >
+            Go to homepage
+          </Button>
+        </Flex>
+        <Show above="sm">
+          <Img
+            src="/images/LeiaFox.webp"
+            alt="Cartoon fox as Princess Leia"
+            height="auto"
+            width={{ base: '42vw', sm: '34vw', md: '28vw', lg: '20vw' }}
+          />
+        </Show>
+      </Flex>
+    </Flex>
+  );
+}
+
+export default Custom404;
