811030b385a8c072a3f6583a21610d3e4521adbf
Linkerin <130695447+Linkerin@users.noreply.github.com>
2023-04-15 17:49:16 +0300

    FIX: returned username api route

---
+++ pages/api/users/[username].ts
@@ -0,0 +1,48 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+import { cleanObjDataTypesForNextJS } from '@/services/utils';
+import getByUser from '@/services/prisma/snippetsService/getByUser';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === 'GET') {
+    const { username, minimal, page } = req.query;
+
+    if (!username) {
+      return res
+        .status(400)
+        .json({ message: '`username` parameter is required' });
+    }
+
+    if (username instanceof Array || minimal instanceof Array) {
+      return res.status(400).json({
+        message:
+          'Inappropriate URL parameters. Only single `username` and `minimal` are allowed'
+      });
+    }
+
+    let minimalInfo = minimal === 'true';
+
+    try {
+      const snippetsData = await getByUser({
+        username,
+        minimalInfo,
+        page
+      });
+      const snippets = snippetsData.map(snippet =>
+        cleanObjDataTypesForNextJS(snippet)
+      );
+
+      return res.json(snippets);
+    } catch (err) {
+      console.error(err);
+      return res
+        .status(500)
+        .json({ message: "Internal error while fetching user's snippets" });
+    }
+  }
+
+  res.status(405).json({ message: 'Method Not Allowed' });
+}
