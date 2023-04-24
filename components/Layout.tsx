68f7539bea80c24145bb56a9e36597d026388d60
Linkerin <130695447+Linkerin@users.noreply.github.com>
2023-04-24 20:26:34 +0300

    UPD: semantic tags for layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,3 +1,5 @@
+import { Suspense } from 'react';
+
 import dynamic from 'next/dynamic';
 import { Grid, GridItem } from '@chakra-ui/react';
 
@@ -30,51 +32,60 @@ function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Meta />
-      <Grid
-        templateAreas={templateAreas}
-        gridTemplateColumns={{
-          base: '1fr',
-          md: '25vw 1fr',
-          lg: '22vw 1fr',
-          xl: '18vw 1fr',
-          '2xl': '15vw 1fr'
-        }}
-        h="100%"
-      >
-        {!isMobile && (
-          <GridItem area="sidebar" zIndex={4}>
-            <SideBar />
-          </GridItem>
-        )}
-        <GridItem
-          cursor="default"
-          maxHeight="100dvh"
-          height="100%"
-          px={{ base: 0, '2xl': 10 }}
+      <Suspense>
+        <Grid
+          templateAreas={templateAreas}
+          gridTemplateColumns={{
+            base: '1fr',
+            md: '25vw 1fr',
+            lg: '22vw 1fr',
+            xl: '18vw 1fr',
+            '2xl': '15vw 1fr'
+          }}
+          h="100%"
         >
-          <GridItem area="topbar" position="sticky" top={0} zIndex={3}>
-            {isMobile ? <MobileTopBar /> : <TopBar />}
-          </GridItem>
-          <GridItem
-            as="main"
-            area="main"
-            h={{
-              base: `calc(100dvh - 58px - ${mobileNavHeightDvh})`,
-              md: 'calc(100dvh - 64px)'
-            }}
-            overflowY="scroll"
-            sx={hideScrollbarCss}
-            pb={2}
-          >
-            {children}
-          </GridItem>
-          {isMobile && (
-            <GridItem as="nav" area="nav" position="sticky" bottom="0px">
-              <MobileNav />
+          {!isMobile && (
+            <GridItem as="aside" area="sidebar" zIndex={4}>
+              <SideBar />
             </GridItem>
           )}
-        </GridItem>
-      </Grid>
+          <GridItem
+            as="section"
+            cursor="default"
+            maxHeight="100dvh"
+            height="100%"
+            px={{ base: 0, '2xl': 10 }}
+          >
+            <GridItem
+              as="header"
+              area="topbar"
+              position="sticky"
+              top={0}
+              zIndex={3}
+            >
+              {isMobile ? <MobileTopBar /> : <TopBar />}
+            </GridItem>
+            <GridItem
+              as="main"
+              area="main"
+              h={{
+                base: `calc(100dvh - 58px - ${mobileNavHeightDvh})`,
+                md: 'calc(100dvh - 64px)'
+              }}
+              overflowY="scroll"
+              sx={hideScrollbarCss}
+              pb={2}
+            >
+              {children}
+            </GridItem>
+            {isMobile && (
+              <GridItem as="nav" area="nav" position="sticky" bottom="0px">
+                <MobileNav />
+              </GridItem>
+            )}
+          </GridItem>
+        </Grid>
+      </Suspense>
       {!isMobile && <AddSnippetButton />}
     </>
   );
