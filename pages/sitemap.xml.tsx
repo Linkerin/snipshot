b7faaa4d3bd4ea1a8be08e6c3ddd6555d2194463
Linkerin <130695447+Linkerin@users.noreply.github.com>
2023-04-26 12:48:04 +0300

    ADD: sitemap.xml

---
+++ pages/sitemap.xml.tsx
@@ -0,0 +1,138 @@
+import { GetServerSideProps } from 'next';
+import { withAxiomGetServerSideProps } from 'next-axiom';
+
+import { LANGS } from '@/services/constants';
+import sitemapServices from '@/services/prisma/sitemapServices';
+
+interface GenerateSiteMapParams {
+  tags: string[];
+  slugs: {
+    slug: string | null;
+    lang: string;
+    updated: Date;
+  }[];
+  users: (string | null)[];
+}
+
+function generateSiteMap({ tags, slugs, users }: GenerateSiteMapParams) {
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL ?? 'https://snipshot.dev';
+
+  return `<?xml version="1.0" encoding="UTF-8"?>
+   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+     <url>
+       <loc>${baseUrl}</loc>
+       <lastmod>2023-04-26T08:28:45.868Z</lastmod>
+       <changefreq>monthly</changefreq>
+       <priority>1.0</priority>
+     </url>
+     <url>
+       <loc>${baseUrl}/about</loc>
+       <lastmod>2023-04-26T08:28:45.868Z</lastmod>
+       <changefreq>monthly</changefreq>
+       <priority>0.9</priority>
+     </url>
+     <url>
+       <loc>${baseUrl}/add</loc>
+       <lastmod>2023-04-26T08:28:45.868Z</lastmod>
+       <changefreq>yearly</changefreq>
+       <priority>0.7</priority>
+     </url>
+     <url>
+       <loc>${baseUrl}/login</loc>
+       <lastmod>2023-04-26T08:28:45.868Z</lastmod>
+       <changefreq>yearly</changefreq>
+       <priority>0.7</priority>
+     </url>
+     ${LANGS.map(lang => {
+       if (lang)
+         return `
+            <url>
+              <loc>${baseUrl}/snippets/${encodeURIComponent(lang)}</loc>
+              <lastmod>${new Date().toISOString()}</lastmod>
+              <changefreq>hourly</changefreq>
+              <priority>1.0</priority>
+            </url>
+        `;
+     }).join('')}
+     ${slugs.map(slugObj => {
+       if (slugObj.slug && slugObj.lang) {
+         return `
+            <url>
+                <loc>${baseUrl}/snippets/${encodeURIComponent(
+           slugObj.lang
+         )}/${encodeURIComponent(slugObj.slug)}</loc>
+                <lastmod>${slugObj.updated.toISOString()}</lastmod>
+                <priority>0.9</priority>
+            </url>
+        `;
+       }
+     })}
+     ${users.map(username => {
+       if (username) {
+         return `
+            <url>
+              <loc>${baseUrl}/users/${encodeURIComponent(username)}</loc>
+              <lastmod>${new Date().toISOString()}</lastmod>
+              <changefreq>daily</changefreq>
+              <priority>0.8</priority>
+            </url>
+        `;
+       }
+     })}
+     ${tags.map(
+       tag => `
+            <url>
+              <loc>${baseUrl}/tags/${encodeURIComponent(tag)}</loc>
+              <lastmod>${new Date().toISOString()}</lastmod>
+              <changefreq>hourly</changefreq>
+              <priority>0.8</priority>
+            </url>
+          `
+     )}
+   </urlset>
+ `;
+}
+
+function SiteMap() {}
+
+export const getServerSideProps: GetServerSideProps =
+  withAxiomGetServerSideProps(async ({ res, log }) => {
+    let slugs: GenerateSiteMapParams['slugs'] = [
+      {
+        slug: null,
+        lang: '',
+        updated: new Date()
+      }
+    ];
+    let tags: GenerateSiteMapParams['tags'] = [''];
+    let users: GenerateSiteMapParams['users'] = [''];
+
+    try {
+      const tagsQuery = sitemapServices.getAllTags();
+      const slugsQuery = sitemapServices.getAllSnippetSlugs();
+      const usersQuery = sitemapServices.getAllUsernames();
+
+      const results = await Promise.all([tagsQuery, slugsQuery, usersQuery]);
+
+      tags = results[0];
+      slugs = results[1];
+      users = results[2];
+    } catch (err) {
+      log.error('Error while fetching data for sitemap.xml', { err });
+    }
+    const sitemap = generateSiteMap({ tags, slugs, users });
+
+    res.setHeader('Content-Type', 'text/xml');
+    res.setHeader(
+      'Cache-Control',
+      'public, s-maxage=6000, stale-while-revalidate=120'
+    );
+    res.write(sitemap);
+    res.end();
+
+    return {
+      props: {}
+    };
+  });
+
+export default SiteMap;
---
+++ services/prisma/sitemapServices.ts
@@ -0,0 +1,30 @@
+import prisma from './prisma';
+
+export async function getAllTags() {
+  const tags = await prisma.snippet.findMany({
+    distinct: ['tags'],
+    select: { tags: true }
+  });
+  const uniqueTags = new Set(tags.map(tagObj => tagObj.tags).flat());
+
+  return new Array(...uniqueTags);
+}
+
+export async function getAllSnippetSlugs() {
+  const slugs = await prisma.snippet.findMany({
+    distinct: ['slug'],
+    select: { slug: true, lang: true, updated: true }
+  });
+
+  return slugs;
+}
+
+export async function getAllUsernames() {
+  const usernames = await prisma.user.findMany({ select: { name: true } });
+
+  return usernames.map(usernameObj => usernameObj.name);
+}
+
+const sitemapServices = { getAllSnippetSlugs, getAllTags, getAllUsernames };
+
+export default sitemapServices;
